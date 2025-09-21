a6fdd68564e2fa2f2066dbbe30d7ff77770f1140
milad998 <myaralbwd20@gmail.com>
2025-09-21 13:39:13 +0300

    Update route.ts

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,7 +1,7 @@
 // src/app/api/auth/register/route.ts
 import { NextRequest, NextResponse } from 'next/server'
 import bcrypt from 'bcryptjs'
-import { prisma } from '@/src/lib/prisma'
+import { prisma } from '../../../../lib/prisma'
 
 export async function POST(request: NextRequest) {
   try {
